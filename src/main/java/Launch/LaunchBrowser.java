4594e184989198c8634614d0a67c28a7078307de
Nirugowda <nirugowda96@gmail.com>
2022-07-19 22:57:16 +0530

    intial commit

---
+++ src/main/java/Launch/LaunchBrowser.java
@@ -0,0 +1,23 @@
+package Launch;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
+public class LaunchBrowser {
+        public static void main(String[] args) throws InterruptedException {
+            // /Users/prashanthkumar/Desktop/Selenium/src/main/resources/chromedriver
+            // D:\Automation\Automation\src\main\resources\chromedriver.exe
+            String path= System.getProperty("user.dir")+ File.separator+"src"+File.separator+"main"+File.separator+
+                    "resources"+ File.separator+ "chromedriver.exe";
+
+            System.setProperty("webdriver.chrome.driver",path);
+            WebDriver driver= new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://chromedriver.chromium.org/");
+            Thread.sleep(3000);
+            driver.close();
+
+        }
+    }
